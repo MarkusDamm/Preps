cc457a9d2583795638b420351117ca2128dc6dad
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2023-05-08 21:33:57 +0200

    generative art

--- L08/script.js
+++ L08/script.js
@@ -4,26 +4,55 @@ var L08;
     document.addEventListener("DOMContentLoaded", hdlLoad);
     let canvas;
     let crc2;
+    // interface Vector2 { x: number, y: number };
     function hdlLoad(_event) {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-        // drawPattern();
+        drawPattern(); //
         drawOtherPattern();
-        drawRec();
+        drawRectangles(10);
     }
-    function drawRec() {
+    function getRandomValue(_min, _max, _hasDecimals = true) {
+        let value = Math.random() * (_max - _min);
+        value += _min;
+        if (!_hasDecimals) {
+            value = Math.floor(value);
+        }
+        return value;
+    }
+    function drawRectangles(_amount) {
+        for (let i = 0; i < _amount; i++) {
+            let x = getRandomValue(0, canvas.width);
+            let y = getRandomValue(0, canvas.width);
+            let w = getRandomValue(0, canvas.width);
+            let h = getRandomValue(0, canvas.width);
+            drawRec(x, y, w, h, createRandomColor());
+        }
+    }
+    /**
+     * Draws a rectangle at the given position with the given color
+     * @param _x X-Position
+     * @param _y Y-Position
+     * @param _w Width
+     * @param _h Heidth
+     * @param _c color
+     */
+    function drawRec(_x, _y, _w, _h, _c) {
+        crc2.save();
         let path = new Path2D();
         crc2.beginPath();
-        path.rect(20, 20, 50, 50);
-        crc2.fillStyle = "#0acf03";
-        crc2.fillRect(20, 20, 50, 50);
+        path.rect(_x, _y, _w, _h);
+        crc2.fillStyle = _c;
+        crc2.fillRect(_x, _y, _w, _h);
         crc2.stroke(path);
+        crc2.restore();
     }
     function drawPattern() {
+        crc2.save();
         let pattern = document.createElement("canvas").getContext("2d");
         pattern.canvas.width = 80;
         pattern.canvas.height = 20;
-        pattern.fillStyle = "#fec";
+        pattern.fillStyle = createRandomColor();
         pattern.fillRect(0, 0, canvas.width / 2, canvas.height);
         pattern.moveTo(0, 10);
         pattern.lineTo(10, 10);
@@ -33,7 +62,7 @@ var L08;
         pattern.lineTo(30, 20);
         pattern.lineTo(20, 20);
         pattern.lineTo(10, 10);
-        pattern.fillStyle = "#111";
+        pattern.fillStyle = createRandomColor();
         pattern.fillRect(canvas.width / 2, 0, canvas.width, canvas.height);
         pattern.moveTo(40, 10);
         pattern.lineTo(50, 10);
@@ -46,8 +75,10 @@ var L08;
         pattern.stroke();
         crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
         crc2.fillRect(0, 0, canvas.width, canvas.height);
+        crc2.restore();
     }
     function drawOtherPattern() {
+        crc2.save();
         let pattern = document.createElement("canvas").getContext("2d");
         pattern.canvas.width = 40;
         pattern.canvas.height = 20;
@@ -59,7 +90,7 @@ var L08;
         path.lineTo(20, 20);
         path.lineTo(10, 20);
         path.lineTo(0, 10);
-        pattern.fillStyle = "blue";
+        pattern.fillStyle = createRandomColor();
         pattern.fill(path);
         pattern.stroke(path);
         path = new Path2D();
@@ -78,10 +109,28 @@ var L08;
         path.lineTo(10, 20);
         path.lineTo(0, 10);
         path.lineTo(0, 20);
-        pattern.fillStyle = "grey";
+        pattern.fillStyle = createRandomColor();
         pattern.fill(path);
         crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
         crc2.fillRect(0, 0, canvas.width, canvas.height);
+        crc2.restore();
+    }
+    function createRandomColor() {
+        let color = "rgb(";
+        let i = 0;
+        while (i < 3) {
+            let value = Math.random() * 256;
+            value = Math.floor(value);
+            color += value;
+            if (i == 2) {
+                color += ")";
+            }
+            else {
+                color += ", ";
+            }
+            i++;
+        }
+        return color;
     }
 })(L08 || (L08 = {}));
 //# sourceMappingURL=script.js.map
--- L08/script.ts
+++ L08/script.ts
@@ -3,32 +3,61 @@ namespace L08 {
 
   let canvas: HTMLCanvasElement;
   let crc2: CanvasRenderingContext2D;
+  // interface Vector2 { x: number, y: number };
 
   function hdlLoad(_event: Event): void {
     canvas = <HTMLCanvasElement>document.querySelector("canvas");
     crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
-    // drawPattern();
+    drawPattern(); //
     drawOtherPattern();
-    drawRec();
+    drawRectangles(10);
   }
 
-  function drawRec(): void {
+  function getRandomValue(_min: number, _max: number, _hasDecimals: boolean = true): number {
+    let value: number = Math.random() * (_max - _min);
+    value += _min;
+    if (!_hasDecimals) {
+      value = Math.floor(value);
+    }
+    return value;
+  }
+
+  function drawRectangles(_amount: number): void {
+    for (let i: number = 0; i < _amount; i++) {
+      let x: number = getRandomValue(0, canvas.width);
+      let y: number = getRandomValue(0, canvas.width);
+      let w: number = getRandomValue(0, canvas.width);
+      let h: number = getRandomValue(0, canvas.width);
+      drawRec(x, y, w, h, createRandomColor());
+    }
+  }
+  /**
+   * Draws a rectangle at the given position with the given color
+   * @param _x X-Position
+   * @param _y Y-Position
+   * @param _w Width
+   * @param _h Heidth
+   * @param _c color
+   */
+  function drawRec(_x: number, _y: number, _w: number, _h: number, _c: string): void {
+    crc2.save();
     let path: Path2D = new Path2D();
     crc2.beginPath();
-    path.rect(20, 20, 50, 50);
+    path.rect(_x, _y, _w, _h);
 
-    crc2.fillStyle = "#0acf03";
-    crc2.fillRect(20, 20, 50, 50);
+    crc2.fillStyle = _c;
+    crc2.fillRect(_x, _y, _w, _h);
     crc2.stroke(path);
-
+    crc2.restore();
   }
 
   function drawPattern(): void {
+    crc2.save();
     let pattern: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.createElement("canvas").getContext("2d");
     pattern.canvas.width = 80;
     pattern.canvas.height = 20;
 
-    pattern.fillStyle = "#fec";
+    pattern.fillStyle = createRandomColor();
     pattern.fillRect(0, 0, canvas.width / 2, canvas.height);
     pattern.moveTo(0, 10);
     pattern.lineTo(10, 10);
@@ -39,7 +68,7 @@ namespace L08 {
     pattern.lineTo(20, 20);
     pattern.lineTo(10, 10);
 
-    pattern.fillStyle = "#111";
+    pattern.fillStyle = createRandomColor();
     pattern.fillRect(canvas.width / 2, 0, canvas.width, canvas.height);
     pattern.moveTo(40, 10);
     pattern.lineTo(50, 10);
@@ -54,9 +83,11 @@ namespace L08 {
 
     crc2.fillStyle = <CanvasPattern>crc2.createPattern(pattern.canvas, "repeat");
     crc2.fillRect(0, 0, canvas.width, canvas.height);
+    crc2.restore();
   }
 
   function drawOtherPattern(): void {
+    crc2.save();
     let pattern: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.createElement("canvas").getContext("2d");
     pattern.canvas.width = 40;
     pattern.canvas.height = 20;
@@ -70,7 +101,7 @@ namespace L08 {
     path.lineTo(10, 20);
     path.lineTo(0, 10);
 
-    pattern.fillStyle = "blue";
+    pattern.fillStyle = createRandomColor();
     pattern.fill(path);
 
     pattern.stroke(path);
@@ -88,19 +119,32 @@ namespace L08 {
     path.lineTo(0, 10);
     path.lineTo(10, 0);
     path.lineTo(0, 0);
-    
+
     path.moveTo(0, 20);
     path.lineTo(10, 20);
     path.lineTo(0, 10);
     path.lineTo(0, 20);
 
-    pattern.fillStyle = "grey";
+    pattern.fillStyle = createRandomColor();
     pattern.fill(path);
 
 
     crc2.fillStyle = <CanvasPattern>crc2.createPattern(pattern.canvas, "repeat");
     crc2.fillRect(0, 0, canvas.width, canvas.height);
+    crc2.restore();
+  }
 
-
+  function createRandomColor(): string {
+    let color: string = "rgb(";
+    let i: number = 0;
+    while (i < 3) {
+      let value: number = Math.random() * 256;
+      value = Math.floor(value);
+      color += value;
+      if (i == 2) { color += ")"; }
+      else { color += ", " }
+      i++;
+    }
+    return color;
   }
 }
